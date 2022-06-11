5e45190ef9ca313d3f99d93eed22b4a3179f70f0
Sadegh Kalate Rahmani <Mehidad.rahmani@gmail.com>
2022-06-11 23:55:07 +0430

    initial Project and Createing Layers,Ready for Init Migration

---
+++ CourseStore.DAL/DbContexts/CourseStoreDbContext.cs
@@ -0,0 +1,33 @@
+﻿using CourseStore.Model.Courses.Entities;
+using CourseStore.Model.Tags.Entities;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace CourseStore.DAL.Contexts;
+
+public class CourseStoreDbContext : DbContext
+{
+
+    public DbSet<Course> Courses { get; set; }
+
+    public DbSet<Tag> Tags { get; set; }
+    public CourseStoreDbContext(DbContextOptions<CourseStoreDbContext> options) : base(options)
+    {
+    }
+  
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(this.GetType().Assembly);
+
+        foreach (var item in modelBuilder.Model.GetEntityTypes())
+        {
+            modelBuilder.Entity(item.ClrType).Property<string>("CreateBy").HasMaxLength(50);
+            modelBuilder.Entity(item.ClrType).Property<string>("UpdateBy").HasMaxLength(50);
+            modelBuilder.Entity(item.ClrType).Property<DateTime>("CreateDate").HasMaxLength(50);
+            modelBuilder.Entity(item.ClrType).Property<DateTime>("UpdateDate").HasMaxLength(50);
+        }
+    }
+
+}
