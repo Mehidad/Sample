6f728b187cf192a5122f81d065f61c8b8fe2e910
Sadegh Kalate Rahmani <Mehidad.rahmani@gmail.com>
2022-06-13 00:48:24 +0430

    Finish CQRS Architecture

--- CourseStore.WebAPI/Program.cs
+++ CourseStore.WebAPI/Program.cs
@@ -1,5 +1,7 @@
+using CourseStore.BLL.Tags.Commands;
 using CourseStore.DAL.Contexts;
 using CourseStore.DAL.Framework;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +13,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<CourseStoreDbContext>(c => c.UseSqlServer(@"Server=localhost\sql2019; Initial Catalog=CleanCourse; User Id=sa; Password=a@12345").
     AddInterceptors(new AddAuditFieldInterceptor()));
+
+builder.Services.AddMediatR(typeof(CreateTagHandler).Assembly);
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
